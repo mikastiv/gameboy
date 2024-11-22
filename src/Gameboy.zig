44d6f08619f71db30a03a8011871ec2afd47f800
mikastiv <mikastiv@outlook.com>
2024-11-22 15:15:40 -0500

    revert timing from frames

--- src/Gameboy.zig
+++ src/Gameboy.zig
@@ -57,9 +57,10 @@ pub fn init(self: *Gameboy) void {
 pub fn run(self: *Gameboy, sdl: SdlContext, tile_viewer: TilesViewer) !void {
     const sec_per_frame = 1.0 / Display.frequency_hz;
     const ns_per_frame = sec_per_frame * std.time.ns_per_s;
+    const clocks_per_frame = Cpu.frequency_hz * sec_per_frame;
 
     var timer = try std.time.Timer.start();
-    var old_frame: u64 = 0;
+    var frames: u64 = 0;
     while (true) {
         var quit = false;
         self.pollEvents(&quit);
@@ -69,8 +70,11 @@ pub fn run(self: *Gameboy, sdl: SdlContext, tile_viewer: TilesViewer) !void {
             self.cpu.step();
             // std.Thread.sleep(std.time.ns_per_ms * 10);
 
-            if (old_frame != self.display.frame_num) {
-                old_frame = self.display.frame_num;
+            const cycles: f64 = @floatFromInt(self.cpu.bus.cycles);
+            const total_frames: u64 = @intFromFloat(cycles / clocks_per_frame);
+
+            if (frames != total_frames) {
+                frames = total_frames;
 
                 const elapsed_ns = timer.read();
                 const expected_ns: u64 = @intFromFloat(ns_per_frame);
