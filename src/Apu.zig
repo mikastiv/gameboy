2740656c0985ff0e5f67f34c6779f564d7560267
mikastiv <mikastiv@outlook.com>
2024-12-02 21:50:30 -0500

    map wave ram

--- src/Apu.zig
+++ src/Apu.zig
@@ -1,6 +1,13 @@
 const Apu = @This();
 
-pub const init: Apu = .{};
+const ram_size = 0x10;
+const ram_mask = ram_size - 1;
+
+ram: [ram_size]u8,
+
+pub const init: Apu = .{
+    .ram = @splat(0),
+};
 
 pub fn read(self: *const Apu, addr: u16) u8 {
     _ = self; // autofix
@@ -12,4 +19,12 @@ pub fn write(self: *Apu, addr: u16, value: u8) void {
     _ = self; // autofix
     _ = addr; // autofix
     _ = value; // autofix
+}
+
+pub fn ramRead(self: *const Apu, addr: u16) u8 {
+    return self.ram[addr & ram_mask];
+}
+
+pub fn ramWrite(self: *Apu, addr: u16, value: u8) void {
+    self.ram[addr & ram_mask] = value;
 }
